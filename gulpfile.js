9a92e69cf33d5e907629110562776230b91fd305
Sergey Ivanenko <sergeianatolich@gmail.com>
2016-10-01 18:57:20 +0300

    frist commit

---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+var gulp = require('gulp'),
+sass = require('gulp-sass'),
+browserSync = require('browser-sync'),
+concat = require('gulp-concat'),
+uglify = require('gulp-uglifyjs'),
+cssnano = require('gulp-cssnano'),
+rename = require('gulp-rename'),
+del = require('del'),
+plumber = require('gulp-plumber'),
+imagemin = require('gulp-imagemin'),
+pngquant = require('imagemin-pngquant'),
+cache = require('gulp-cache'),
+autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function() {
+	return gulp.src(['src/sass/**/*.sass', 'src/sass/**/*.scss'])
+	.pipe(plumber({
+		errorHandler: function (err) {
+			console.log(err);
+			this.emit('end');
+		}
+	}))
+	.pipe(sass())
+	.pipe(gulp.dest('src/css'))
+	.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('scripts', function() {
+	return gulp.src('src/plugins/*.js')
+	.pipe(concat('plugins.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('src/js'));
+});
+
+gulp.task('css-libs', ['sass'], function() {
+	return gulp.src('src/css/**/*')
+	.pipe(cssnano())
+	.pipe(rename({suffix: '.min'}))
+	.pipe(gulp.dest('src/css'));
+});
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'src'
+		},
+		notify: false
+	});
+});
+
+gulp.task('clean', function() {
+	return del.sync('dist');
+});
+
+gulp.task('clear', function() {
+	return cache.clearAll();
+});
+
+gulp.task('img' , function() {
+	return gulp.src('src/img/**/*')
+	.pipe(cache(imagemin({
+			interlaced: true,
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		})))
+	.pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('watch', ['browser-sync', 'sass', 'scripts'], function() {
+	gulp.watch(['src/sass/**/*.sass', 'src/sass/**/*.scss'], ['sass']);
+	gulp.watch('src/*.html', browserSync.reload);
+	gulp.watch('src/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('build', ['clean', 'img', 'sass', 'scripts'], function() {
+	var buildCss = gulp.src('src/css/*.css')
+	.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+	.pipe(concat('style.min.css'))
+	.pipe(cssnano())
+	.pipe(gulp.dest('dist/css'));
+
+	var buildFonts = gulp.src('src/fonts/**/*')
+	.pipe(gulp.dest('dist/fonts'));
+
+	var buildJs = gulp.src('src/js/**/*')
+	.pipe(concat('script.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('dist/js'));
+
+	var buildHtml = gulp.src('src/*.html')
+	.pipe(gulp.dest('dist'));
+});
